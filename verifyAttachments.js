f9bb3efcdf70868aa8b4b4ec8417527595f42c60
Yako0 <34353157+Yako0@users.noreply.github.com>
2023-09-13 22:57:54 +0300

    Update verifyAttachments.js

--- verifyAttachments.js
+++ verifyAttachments.js
@@ -8,10 +8,15 @@ console.log('btn1');
 	if(saveCreateAP!=null){
 		console.log('saveCreateAP');
 saveCreateAP.addEventListener('click', ()=>{
+	console.log('saveCreateAP 1');
 		var attachment=document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:r2:0:a1:aapg_link');
+	console.log('saveCreateAP 2 ' );
 		var attachmentTbl=attachment.firstChild;
+	console.log('saveCreateAP 3 ');
 		var attachmentFirstRow=attachmentTbl.firstChild;
+	console.log('saveCreateAP 4 ');
 		var attachmentFirstRowFirstData=attachmentFirstRow.firstChild;
+	console.log('saveCreateAP 5 ');
 		console.log(attachmentFirstRowFirstData);
 		if(attachmentFirstRowFirstData != null){
 		 
