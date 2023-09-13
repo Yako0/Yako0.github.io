0de3309282d68baf1eeecef2876e52ab35f2736d
Yako0 <34353157+Yako0@users.noreply.github.com>
2023-09-13 21:07:22 +0300

    Add files via upload

---
+++ verifyAttachments.js
@@ -0,0 +1,46 @@
+function saveManageAP(){
+		var attachment=document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:r2:0:a1:aapg_link');
+		var attachmentTbl=attachment.firstChild;
+		var attachmentFirstRow=attachmentTbl.firstChild;
+		var attachmentFirstRowFirstData=attachmentFirstRow.firstChild;
+		console.log(attachmentFirstRowFirstData);
+		if(attachmentFirstRowFirstData != null){
+		 
+		var noAttachments=attachmentFirstRowFirstData.innerText;
+		console.log(noAttachments);
+		alert('Attachment is required');}
+		 
+		  
+		else{var divBtn= document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:ct5');
+		var aSave = divBtn.firstChild;
+		console.log(aSave);
+		if(aSave!=null){
+		aSave.click();
+		}
+		}
+		};
+		function saveCreateAP(){
+		var attachment=document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:r2:0:a1:aapg_link');
+		var attachmentTbl=attachment.firstChild;
+		var attachmentFirstRow=attachmentTbl.firstChild;
+		var attachmentFirstRowFirstData=attachmentFirstRow.firstChild;
+		console.log(attachmentFirstRowFirstData);
+		if(attachmentFirstRowFirstData != null){
+		 
+		var noAttachments=attachmentFirstRowFirstData.innerText;
+		console.log(noAttachments);
+		alert('Attachment is required');}
+		 
+		  
+		else{var divBtn= document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:ct5');
+		var aSave = divBtn.firstChild;
+		console.log(aSave);
+		if(aSave!=null){
+		aSave.click();
+		}
+		}
+		};
+		function testMe(){
+			
+			alert('I am OK!');
+		};
