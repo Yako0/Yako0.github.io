4c7f38787b9704143e2cceba038f3fa5e9716d51
Yako0 <34353157+Yako0@users.noreply.github.com>
2023-09-13 22:15:38 +0300

    Update verifyAttachments.js

--- verifyAttachments.js
+++ verifyAttachments.js
@@ -1,5 +1,25 @@
 var saveCreateAP=document.getElementById('saveCreateAP');
-saveCreateAP.addEventListener('click', saveCreateAP);
+saveCreateAP.addEventListener('click', ()=>{
+		var attachment=document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:r2:0:a1:aapg_link');
+		var attachmentTbl=attachment.firstChild;
+		var attachmentFirstRow=attachmentTbl.firstChild;
+		var attachmentFirstRowFirstData=attachmentFirstRow.firstChild;
+		console.log(attachmentFirstRowFirstData);
+		if(attachmentFirstRowFirstData != null){
+		 
+		var noAttachments=attachmentFirstRowFirstData.innerText;
+		console.log(noAttachments);
+		alert('Attachment is required');}
+		 
+		  
+		else{var divBtn= document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:ct5');
+		var aSave = divBtn.firstChild;
+		console.log(aSave);
+		if(aSave!=null){
+		aSave.click();
+		}
+		}
+		});
 function saveManageAP(){
 		var attachment=document.getElementById('pt1:_FOr1:0:_FONSr2:0:MAnt2:0:pm1:r1:0:ap1:r2:0:a1:aapg_link');
 		var attachmentTbl=attachment.firstChild;
